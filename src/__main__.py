a560f98b17060b56052b6b5b53968daea78c4ec8
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-09-06 14:42:21 -0500

    Creating main run file.

---
+++ src/__main__.py
@@ -0,0 +1,17 @@
+""""This files is the main file of the bot"""
+
+from pyrogram.client import Client
+from decouple import config
+
+PLUGINS = dict(root='src/plugins')
+
+app = Client(
+    "FplBot",
+    api_id=config("API_ID"),
+    api_hash=config("API_HASH"),
+    bot_token=config("BOT_TOKEN"),
+    plugins=PLUGINS
+)
+
+if __name__ == "__main__":
+    app.run()
